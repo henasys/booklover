e38c781421161578892e649fab15e4b7b097db95
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-02-19 12:47:19 +0900

    rename BookLover3

--- android/app/src/main/java/com/henasys/app/booklover/MainActivity.java
+++ android/app/src/main/java/com/henasys/app/booklover/MainActivity.java
@@ -1,4 +1,4 @@
-package com.booklover;
+package com.henasys.app.booklover;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "BookLover";
+    return "BookLover3";
   }
 }
