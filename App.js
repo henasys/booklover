0cdc92680b03fa407a8d5b218251c1c46865ca60
AlexLee <alexhojonglee@gmail.com>
2020-05-04 07:15:59 +0900

    build for react-native-camera

--- App.js
+++ App.js
@@ -1,39 +1,76 @@
-import React, {useState, useEffect} from 'react';
-import {Linking, StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+'use strict';
+import React, {PureComponent} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-const App = () => {
-  const [url, setUrl] = useState(null);
-  const handleUrl = event => {
-    console.log('handleUrl', event);
-    setUrl(event.url);
+class App extends PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({barcodes}) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}>
+            <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
   };
-
-  useEffect(() => {
-    Linking.getInitialURL()
-      .then(initialUrl => {
-        console.log('getInitialURL', initialUrl);
-        setUrl(initialUrl);
-      })
-      .catch(e => {
-        console.log('getInitialURL error', e);
-      });
-    Linking.addEventListener('url', handleUrl);
-    console.log('Linking.addEventListener');
-
-    return () => {
-      Linking.removeEventListener('url', handleUrl);
-      console.log('Linking.removeEventListener');
-    };
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Text>{`The deep link is: ${url}`}</Text>
-    </View>
-  );
-};
+}
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
 });
