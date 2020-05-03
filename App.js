4d0c1c1d179a19d2e5098134f9deb59fcebf6328
AlexLee <alexhojonglee@gmail.com>
2020-05-04 07:27:16 +0900

    change to not use audio

--- App.js
+++ App.js
@@ -15,15 +15,10 @@ class App extends PureComponent {
           style={styles.preview}
           type={RNCamera.Constants.Type.back}
           flashMode={RNCamera.Constants.FlashMode.on}
+          captureAudio={false}
           androidCameraPermissionOptions={{
             title: 'Permission to use camera',
             message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
             buttonPositive: 'Ok',
             buttonNegative: 'Cancel',
           }}
