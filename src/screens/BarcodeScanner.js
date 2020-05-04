8014ed9349f99c62136d193ac989617504ef9f90
AlexLee <alexhojonglee@gmail.com>
2020-05-04 15:15:48 +0900

    make BarcodeScanner

--- src/screens/BarcodeScanner.js
+++ src/screens/BarcodeScanner.js
@@ -1,46 +1,65 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {RNCamera} from 'react-native-camera';
-import {BarcodeMask} from '@nartc/react-native-barcode-mask';
+import {BarcodeMaskWithOuterLayout} from '@nartc/react-native-barcode-mask';
 
-class BarcodeScanner extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          captureAudio={false}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onBarCodeRead={event => {
-            console.log('onBarCodeRead', event);
-          }}>
-          <BarcodeMask />
-        </RNCamera>
+const defaultBarCodeTypes = [
+  RNCamera.Constants.BarCodeType.ean13,
+  RNCamera.Constants.BarCodeType.ean8,
+];
+
+function BarcodeScanner(props) {
+  const [barcode, setBarcode] = useState(null);
+  const onBarcodeRead = event => {
+    console.log('onBarcodeRead', event);
+    setBarcode(event.data);
+  };
+  const barCodeTypes = barcode ? [] : defaultBarCodeTypes;
+  return (
+    <SafeAreaView style={styles.container}>
+      <RNCamera
+        androidCameraPermissionOptions={{
+          title: 'permissionCamera',
+          message: 'permissionCameraMessage',
+          buttonPositive: 'ok',
+          buttonNegative: 'cancel',
+        }}
+        style={styles.scanner}
+        type={RNCamera.Constants.Type.back}
+        barCodeTypes={barCodeTypes}
+        onBarCodeRead={onBarcodeRead}
+        captureAudio={false}>
+        <BarcodeMaskWithOuterLayout
+          maskOpacity={0.5}
+          width={'80%'}
+          height={100}
+          showAnimatedLine={barcode === null}
+        />
+      </RNCamera>
+      <View style={styles.listContainer}>
+        <Text>Barcode: {barcode}</Text>
       </View>
-    );
-  }
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+    // alignItems: 'center',
     backgroundColor: 'black',
   },
-  preview: {
+  scanner: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    // justifyContent: 'flex-end',
+    // alignItems: 'center',
+    height: 150,
+  },
+  listContainer: {
+    flex: 2,
+    backgroundColor: 'white',
   },
 });
 
