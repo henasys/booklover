f7eead82171bb4e5ffedf832027195d9c218a974
AlexLee <alexhojonglee@gmail.com>
2020-05-06 13:27:42 +0900

    change ActionButton label and icon

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -2,57 +2,40 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {FloatingAction} from 'react-native-floating-action';
-import Aladin from '../modules/Aladin';
+import {Icon} from 'react-native-elements';
 
-const actions = [
-  {
-    text: 'Accessibility',
-    icon: <Icon name="md-create" />,
-    name: 'bt_accessibility',
-    position: 2,
-  },
-  {
-    text: 'Language',
-    icon: <Icon name="md-create" />,
-    name: 'bt_language',
-    position: 1,
-  },
-  {
-    text: 'Location',
-    icon: <Icon name="md-create" />,
-    name: 'bt_room',
-    position: 3,
-  },
-  {
-    text: 'Video',
-    icon: <Icon name="md-create" />,
-    name: 'bt_videocam',
-    position: 4,
-  },
-];
+import Aladin from '../modules/Aladin';
 
 function Main() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Main Screen</Text>
       <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-            <Icon name="md-create" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-            <Icon name="md-done-all" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-        </ActionButton>
+        <ActionButton.Item
+          buttonColor="#3498db"
+          title="제목 검색"
+          onPress={() => {}}>
+          <Icon name="search" type="material" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#1abc9c"
+          title="바코드 검색"
+          onPress={() => {}}>
+          <Icon
+            name="barcode-scan"
+            type="material-community"
+            style={styles.actionButtonIcon}
+          />
+        </ActionButton.Item>
+      </ActionButton>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   actionButtonIcon: {
     fontSize: 20,
     height: 22,
