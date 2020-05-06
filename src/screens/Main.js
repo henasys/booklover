cedb1c0f7776ce538dc9c23cb1733ded75e40fdc
AlexLee <alexhojonglee@gmail.com>
2020-05-06 13:13:17 +0900

    use patched react-native-action-button

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -37,13 +37,17 @@ function Main() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Main Screen</Text>
-      <FloatingAction
-        actions={actions}
-        position="right"
-        onPressItem={name => {
-          alert('Icon pressed', `the icon ${name} was pressed`);
-        }}
-      />
+      <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
     </View>
   );
 }
