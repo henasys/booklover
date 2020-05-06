4cd7608fdc1dacb885b47063356cebafe38b3317
AlexLee <alexhojonglee@gmail.com>
2020-05-06 11:17:49 +0900

    test ActionButton

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,6 +1,8 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Aladin from '../modules/Aladin';
 
@@ -8,25 +10,36 @@ function Main() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Main Screen</Text>
-      <Button
-        title="search ISBN"
-        onPress={() => {
-          // const isbn = 'K882531540';
-          const isbn = '9788994803548';
-          const searcher = new Aladin();
-          searcher
-            .searchIsbn(isbn)
-            .then(response => {
-              console.log('searchIsbn response', response);
-              console.log('searchIsbn response.item', response.item);
-            })
-            .catch(e => {
-              console.log('searchIsbn error', e);
-            });
-        }}
-      />
+      <ActionButton buttonColor="rgba(231,76,60,1)">
+        <ActionButton.Item
+          buttonColor="#9b59b6"
+          title="New Task"
+          onPress={() => console.log('notes tapped!')}>
+          <Icon name="md-create" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#3498db"
+          title="Notifications"
+          onPress={() => {}}>
+          <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item
+          buttonColor="#1abc9c"
+          title="All Tasks"
+          onPress={() => {}}>
+          <Icon name="md-done-all" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
 
 export default Main;
