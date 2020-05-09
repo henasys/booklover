59cf99e8a966230222528e079d39786598d89cf8
AlexLee <alexhojonglee@gmail.com>
2020-05-09 16:20:41 +0900

    add Util.parseInt

--- src/modules/util.js
+++ src/modules/util.js
@@ -1,3 +1,13 @@
 export function splitCategoryName(categoryName) {
-  return categoryName.split('>');
+  return categoryName ? categoryName.split('>') : [];
 }
+
+export function parseInt(value) {
+  console.log('parseInt', value, typeof value);
+  return typeof value === 'string' ? parseInt(value, 10) : value;
+}
+
+export default {
+  splitCategoryName,
+  parseInt,
+};
