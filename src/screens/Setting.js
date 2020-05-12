c5764838bdb1d867a9d7fa7cd95718210f0c962e
AlexLee <alexhojonglee@gmail.com>
2020-05-12 23:48:33 +0900

    apply DocumentPicker

--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -6,6 +6,8 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {Button, Icon, Input} from 'react-native-elements';
 import Toast from 'react-native-simple-toast';
 import ProgressBar from 'react-native-progress/Bar';
+import DocumentPicker from 'react-native-document-picker';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 import Database from '../modules/database';
 // import Permission from '../modules/permission';
@@ -29,13 +31,34 @@ const write = (fileName, content) => {
     });
 };
 
+const pickFile = async setValue => {
+  try {
+    const res = await DocumentPicker.pick({
+      type: [DocumentPicker.types.plainText],
+    });
+    console.log(
+      res.uri,
+      res.type, // mime type
+      res.name,
+      res.size,
+    );
+    setValue(res.name);
+  } catch (err) {
+    if (DocumentPicker.isCancel(err)) {
+      // User cancelled the picker, exit any dialogs or menus and move on
+    } else {
+      throw err;
+    }
+  }
+};
+
 function Setting({navigation, route}) {
   const [realm, setRealm] = useState(null);
   const [fileName, setFileName] = useState('booklover-backup.xlsx');
   const [fileNameError, setFileNameError] = useState(null);
   const [progress, setProgress] = useState(0);
   const [showProgress, setShowProgress] = useState(false);
-  const [isbnFile, setIsbnFile] = useState('booklover-isbn.txt');
+  const [isbnFile, setIsbnFile] = useState('');
   useEffect(() => {
     Database.open(_realm => {
       setRealm(_realm);
@@ -139,24 +162,33 @@ function Setting({navigation, route}) {
           <View style={styles.spacer} />
           <View style={styles.spacer} />
           <View style={styles.spacer} />
-          <Input
-            containerStyle={styles.textInputBox}
-            labelStyle={{fontSize: 14}}
-            onChangeText={setIsbnFile}
-            onEndEditing={onEndEditingFileName}
-            defaultValue={isbnFile}
-            label={'ISBN 파일'}
-            keyboardType="default"
-            autoCapitalize="none"
-            multiline={true}
-            numberOfLines={1}
-          />
+          <TouchableOpacity
+            onPress={() => {
+              console.log('onPress TouchableOpacity');
+              pickFile(setIsbnFile);
+            }}>
+            <Input
+              onPress={() => {
+                console.log('onPress Input');
+              }}
+              disabledInputStyle={{color: 'black', opacity: 1}}
+              containerStyle={styles.textInputBox}
+              labelStyle={{fontSize: 14}}
+              defaultValue={isbnFile}
+              label={'ISBN 파일'}
+              keyboardType="default"
+              autoCapitalize="none"
+              multiline={true}
+              numberOfLines={1}
+            />
+          </TouchableOpacity>
+
           <Button
             title="ISBN 검색 추가"
             type="outline"
             icon={<Icon name="save" type="material" />}
             onPress={() => {
-              //
+             //
             }}
           />
           <View style={styles.spacer} />
