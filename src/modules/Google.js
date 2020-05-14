28f2bb42fa2daa75b3c3ce488ad4b8868f07fc9d
AlexLee <alexhojonglee@gmail.com>
2020-05-14 21:56:00 +0900

    add Google API

---
+++ src/modules/Google.js
@@ -0,0 +1,30 @@
+const IsbnUtil = require('isbn-utils');
+
+class Google {
+  getUrlForIsbn(p) {
+    return `https://www.googleapis.com/books/v1/volumes?q=isbn:${p.isbn}`;
+  }
+
+  searchIsbn(isbn) {
+    const params = {
+      isbn: isbn,
+    };
+    const url = this.getUrlForIsbn(params);
+    // console.log('searchIsbn url', url);
+    return this.fetch(url);
+  }
+
+  fetch(url) {
+    console.debug('fetch url', url);
+    return fetch(url)
+      .then(response => {
+        // console.log('response', response);
+        return response.json();
+      })
+      .then(response => {
+        console.log('response.json', response);
+      });
+  }
+}
+
+export default Google;
