cbd7cc9b3d60e85e4e9585c0c2b8d5d1571fe327
AlexLee <alexhojonglee@gmail.com>
2020-05-16 09:59:51 +0900

    add pickerSelect for API source

--- src/modules/myColor.js
+++ src/modules/myColor.js
@@ -1,7 +1,11 @@
 const font1 = 'steelblue';
+const font2 = '#333333';
 const bg1 = '#eee';
+const line1 = '#bbbbbb';
 
 export default {
   font1,
+  font2,
   bg1,
+  line1,
 };
--- src/screens/Setting.js
+++ src/screens/Setting.js
@@ -1,15 +1,51 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, ScrollView, View} from 'react-native';
+import {StyleSheet, ScrollView, View, Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Button, Icon} from 'react-native-elements';
+import RNPickerSelect from 'react-native-picker-select';
 
 import Database from '../modules/database';
 import MyAlert from '../views/alert';
+import MyColor from '../modules/myColor';
+
+const pickerApiSource = (realm, apiSource, setApiSource) => {
+  const data = [
+    {label: '네이버', value: Database.Setting.apiSourceType.NAVER},
+    {label: '알라딘', value: Database.Setting.apiSourceType.ALADIN},
+  ];
+  return (
+    <View
+      style={{
+        borderWidth: 0.5,
+        borderRadius: 4,
+        borderColor: MyColor.line1,
+      }}>
+      <RNPickerSelect
+        placeholder={{}}
+        style={pickerSelectStyles}
+        value={apiSource}
+        items={data}
+        useNativeAndroidPickerStyle={true}
+        pickerProps={{color: 'blue'}}
+        onValueChange={(value, index) => {
+          console.log('onValueChange', value, index);
+          setApiSource(value);
+          Database.saveSetting(realm, value)
+            .then(setting => {
+              console.log('saveSetting done', setting.apiSource);
+            })
+            .catch(e => {});
+        }}
+      />
+    </View>
+  );
+};
 
 function Setting({navigation}) {
   const [realm, setRealm] = useState(null);
-  const [] = useState('');
+  const [apiSource, setApiSource] = useState(null);
   useEffect(() => {
     Database.open(_realm => {
       setRealm(_realm);
@@ -20,10 +56,20 @@ function Setting({navigation}) {
       // console.log('Database.close');
     };
   }, []);
+  useEffect(() => {
+    if (!realm) {
+      return;
+    }
+    const setting = Database.getSetting(realm);
+    setApiSource(setting.apiSource);
+  }, [realm]);
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.contentContainer}>
         <View>
+          <Text>검색 API</Text>
+          <View style={styles.spacer} />
+          {pickerApiSource(realm, apiSource, setApiSource)}
           <View style={styles.spacer} />
           <View style={styles.spacer} />
           <View style={styles.spacer} />
@@ -93,6 +139,29 @@ const styles = StyleSheet.create({
   },
   spacer: {
     paddingVertical: 5,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 0.5,
+    borderColor: MyColor.line1,
+    borderRadius: 4,
+    color: MyColor.font2,
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: MyColor.line1,
+    borderRadius: 2,
+    color: MyColor.font2,
+    paddingRight: 30, // to ensure the text is never behind the icon
   },
 });
 
