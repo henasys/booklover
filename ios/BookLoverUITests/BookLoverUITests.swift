cb1391e6fd51a99a21c520fcdc3594385371ba80
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-05-22 07:46:26 +0900

    add for snapshot

--- ios/BookLoverUITests/BookLoverUITests.swift
+++ ios/BookLoverUITests/BookLoverUITests.swift
@@ -10,6 +10,14 @@ import XCTest
 
 class BookLoverUITests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+  
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -24,20 +32,18 @@ class BookLoverUITests: XCTestCase {
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+        snapshot("0Launch")
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
