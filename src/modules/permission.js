be553a3bb440854f52b45fc339d3735a88f1cf0c
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-05-22 16:11:34 +0900

    add errorCallback

--- src/modules/permission.js
+++ src/modules/permission.js
@@ -1,11 +1,12 @@
 import {Platform} from 'react-native';
 import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
-const checkPermission = (permission, callback = null) => {
+const checkPermission = (permission, callback = null, errorCallbak = null) => {
   check(permission)
     .then(result => {
       switch (result) {
         case RESULTS.UNAVAILABLE:
+          errorCallbak(RESULTS.UNAVAILABLE);
           console.log(
             'This feature is not available (on this device / in this context)',
           );
@@ -21,6 +22,7 @@ const checkPermission = (permission, callback = null) => {
           callback && callback();
           break;
         case RESULTS.BLOCKED:
+          errorCallbak(RESULTS.BLOCKED);
           console.log('The permission is denied and not requestable anymore');
           break;
       }
@@ -30,16 +32,22 @@ const checkPermission = (permission, callback = null) => {
     });
 };
 
-const requestPermission = (permission, callback = null) => {
+const requestPermission = (
+  permission,
+  callback = null,
+  errorCallbak = null,
+) => {
   request(permission)
     .then(result => {
       switch (result) {
         case RESULTS.UNAVAILABLE:
+          errorCallbak(RESULTS.UNAVAILABLE);
           console.log(
             'This feature is not available (on this device / in this context)',
           );
           break;
         case RESULTS.DENIED:
+          errorCallbak(RESULTS.DENIED);
           console.log(
             'The permission has not been requested / is denied but requestable',
           );
@@ -49,6 +57,7 @@ const requestPermission = (permission, callback = null) => {
           callback && callback();
           break;
         case RESULTS.BLOCKED:
+          errorCallbak(RESULTS.BLOCKED);
           console.log('The permission is denied and not requestable anymore');
           break;
       }
@@ -58,24 +67,48 @@ const requestPermission = (permission, callback = null) => {
     });
 };
 
-const checkPermissionForWriteExternalStorage = (callback = null) => {
-  checkPermission(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE, callback);
+const checkPermissionForWriteExternalStorage = (
+  callback = null,
+  errorCallbak = null,
+) => {
+  checkPermission(
+    PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+    callback,
+    errorCallbak,
+  );
 };
 
-const checkPermissionForReadExternalStorage = (callback = null) => {
-  checkPermission(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE, callback);
+const checkPermissionForReadExternalStorage = (
+  callback = null,
+  errorCallbak = null,
+) => {
+  checkPermission(
+    PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+    callback,
+    errorCallbak,
+  );
 };
 
-const checkPermissionForCoarseLocation = (callback = null) => {
-  checkPermission(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION, callback);
+const checkPermissionForCoarseLocation = (
+  callback = null,
+  errorCallbak = null,
+) => {
+  checkPermission(
+    PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+    callback,
+    errorCallbak,
+  );
 };
 
-const checkPermissionForFineLocation = (callback = null) => {
+const checkPermissionForFineLocation = (
+  callback = null,
+  errorCallbak = null,
+) => {
   const permission = Platform.select({
     android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
     ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
   });
-  checkPermission(permission, callback);
+  checkPermission(permission, callback, errorCallbak);
 };
 
 export default {
