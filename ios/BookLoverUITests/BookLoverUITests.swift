20e467933e126548547a0dd37d5771ef5b483e57
Alex Hojong Lee <alexhojonglee@gmail.com>
2020-05-26 08:04:46 +0900

    add setUp

--- ios/BookLoverUITests/BookLoverUITests.swift
+++ ios/BookLoverUITests/BookLoverUITests.swift
@@ -22,14 +22,21 @@ class BookLoverUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+  
+    override func setUp() {
+        super.setUp()
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
         // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+              // Use XCTAssert and related functions to verify your tests produce the correct results.
+      snapshot("01LoginScreen")
+                  
+                                                            
     }
 
     func testLaunchPerformance() throws {
